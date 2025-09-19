4e8d0e6e6a2e4f8de26c373cf18c86e6ba7b9e5c
Alexandre <alevaristofig@gmail.com>
2025-09-18 21:03:37 -0300

    Criação do projeto AlgaPostProcessorService

---
+++ src/main/java/com/postprocessorservice/PostProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.postprocessorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostProcessorServiceApplication.class, args);
+	}
+
+}
